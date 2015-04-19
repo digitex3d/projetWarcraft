1329e76a097ed9b3819bed75a24ffaa5557ac64c
digitex3d <digitex3d@gmail.com>
2015-04-19 12:54:52 +0200

    IVillageois

---
+++ src/services/IVillageois.java
@@ -0,0 +1,45 @@
+package services;
+
+import enums.ERace;
+
+
+
+public interface IVillageois {
+
+	//		Observators
+	ERace 	getRace();
+	int 	getLargeur();
+	int		getHauteur();
+	int		getForce();
+	double	getVitesse();
+	int 	getPointsDeVie();
+	int		getQuantiteOr();
+	boolean	estMort();
+	
+	//		Constructors
+	IVillageois init(ERace race, int largeur, int hauteur, int force, 
+			double vitesse, int pointsVie);
+	
+	/* pre 	pre init(race,largeur,hauteur,force,vitesse,pointsVie) 
+	* 	require 	largeur%2=1 ∧ 
+	* 				hauteur%2=1 ∧
+	* 				force, vitesse, pointsVie > 0
+	*/
+
+	
+	
+	//Operators
+	// pre retrait(V,s) require ¬estMort(V) ∧ s>0
+	
+	void retrait(int pointsVie);
+	
+	/* post
+	 * 	pointsDeVie(retrait(V,s))=pointsDeVie(V) -s
+	 *	quantiteOr(retrait(V,s))=quantiteOr(V)
+	 */
+	
+	
+	//Invariants
+	// estMort(V) min = pointsDeVie(V) ≤ 0
+	
+}
