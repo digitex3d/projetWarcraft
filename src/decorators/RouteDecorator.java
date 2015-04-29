84f66ce69fcb2a996822660d74ec32cf2d161109
digitex3d <digitex3d@gmail.com>
2015-04-29 15:15:59 +0200

    Route Decorator

---
+++ src/decorators/RouteDecorator.java
@@ -0,0 +1,56 @@
+package decorators;
+
+import services.IRoute;
+
+public class RouteDecorator implements IRoute {
+	protected IRoute delegate;
+	
+	public RouteDecorator(IRoute delegate) {
+
+		this.delegate = delegate;
+		
+	}
+
+	@Override
+	public int getX() {
+		return this.delegate.getX();
+	}
+
+	@Override
+	public int getY() {
+		return this.delegate.getY();
+	}
+
+	@Override
+	public void setX(int x) {
+		this.delegate.setX(x);
+	}
+
+	@Override
+	public void setY(int y) {
+		this.delegate.setY(y);
+
+	}
+
+	@Override
+	public int getLargeur() {
+		
+		return this.delegate.getLargeur();
+	}
+
+	@Override
+	public int getHauteur() {
+		return this.delegate.getHauteur();
+	}
+
+	@Override
+	public IRoute init(int x, int y, int largeur, int hauteur, int mult) {
+		return this.delegate.init(x, y, largeur, hauteur, mult);
+	}
+
+	@Override
+	public int getMult() {
+		return this.delegate.getMult();
+	}
+
+}
