d4e8b38a4296625a7842e5b4743f43efe469e0ae
Daniele Siragusa <dnl.siragusa@gmail.com>
2015-04-30 17:28:59 +0200

    Aggiunte implementazioni buggate, corretti bug in Villageois e Mine (minimizzazioni)

---
+++ src/implementations/MurailleImpl.java
@@ -0,0 +1,69 @@
+package implementations;
+
+import services.IMuraille;
+
+public class MurailleImpl implements IMuraille {
+	private int largeur;
+	private int hauteur;
+	private int x;
+	private int y;
+	private int pointsDeVie;
+	
+	@Override
+	public int getLargeur() {
+		return largeur;
+	}
+
+	@Override
+	public int getHauteur() {
+		return hauteur;
+	}
+
+	@Override
+	public int getX() {
+		return x;
+	}
+
+	@Override
+	public int getY() {
+		return y;
+	}
+
+	@Override
+	public int getPointsDeVie() {
+		return pointsDeVie;
+	}
+
+	@Override
+	public boolean estDetruite() {
+		return pointsDeVie <= 0;
+	}
+
+	@Override
+	public IMuraille init(int x, int y, int largeur, int hauteur, int pointsVie) {
+		this.x = x;
+		this.y = y;
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+		this.pointsDeVie = pointsVie;
+		return this;
+	}
+
+	@Override
+	public void retrait(int s) {
+		this.pointsDeVie -= s;
+	}
+
+	@Override
+	public void setX(int x) {
+		this.x = x;
+		
+	}
+
+	@Override
+	public void setY(int y) {
+		this.y = y;
+		
+	}
+
+}
--- src/implementations/VillageoisImpl.java
+++ src/implementations/VillageoisImpl.java
@@ -12,7 +12,6 @@ public class VillageoisImpl implements IVillageois {
 	protected double vitesse;
 	protected int pointsDeVie;
 	protected int quantiteOr;
-    protected boolean estMort;
     protected int posX;
     protected int posY;
     
@@ -75,7 +74,7 @@ public class VillageoisImpl implements IVillageois {
 
 	@Override
 	public boolean estMort() {
-		return this.estMort;
+		return this.pointsDeVie <= 0;
 		
 	}
 	
