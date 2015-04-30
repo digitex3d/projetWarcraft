9f6f247fa518e9a610421c1f6ad5857446bffe94
digitex3d <digitex3d@gmail.com>
2015-04-30 12:28:34 +0200

    Decoratore HV

---
+++ src/decorators/HotelVilleDecorator.java
@@ -0,0 +1,59 @@
+package decorators;
+
+import services.IHotelVille;
+
+public class HotelVilleDecorator implements IHotelVille{
+	private IHotelVille delegate;
+
+	public HotelVilleDecorator(IHotelVille delegate) {
+		super();
+		this.delegate = delegate;
+	}
+
+	@Override
+	public int getLargeur() {
+		return this.delegate.getLargeur();
+	}
+
+	@Override
+	public int getHauteur() {
+		return this.delegate.getHauteur();
+	}
+
+	@Override
+	public int getOrRestant() {
+		return this.delegate.getOrRestant();
+	}
+
+	@Override
+	public int getX() {
+		return this.delegate.getX();
+	}
+
+	@Override
+	public int getY() {
+		return this.delegate.getY();
+	}
+
+	@Override
+	public void setX(int x) {
+		this.delegate.setX(x);		
+	}
+
+	@Override
+	public void setY(int y) {
+		this.delegate.setY(y);		
+		
+	}
+
+	@Override
+	public void depot(int s) {
+		this.delegate.depot(s);
+	}
+
+	@Override
+	public IHotelVille init(int x, int y, int largeur, int hauteur, int or) {
+		return this.init(x, y, largeur, hauteur, or);
+	}
+
+}
