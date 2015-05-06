5e220d269a63efaee95d6da7e60d9e604d050392
digitex3d <digitex3d@gmail.com>
2015-05-06 16:49:52 +0200

    aggiunta setXY invece di setx e sety

--- src/services/IEntite.java
+++ src/services/IEntite.java
@@ -3,8 +3,7 @@ package services;
 public interface IEntite {
 	public int getX();
 	public int getY();
-	public void setX(int x);
-	public void setY(int y);
+	public void setXY(int x, int y);
 	public int getLargeur();
 	public int getHauteur();
 	
