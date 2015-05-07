6134c505d94b94f77bca34e6d1f035764e4e76b8
digitex3d <digitex3d@gmail.com>
2015-05-07 21:01:43 +0200

    Implementazione Motore in corso...aggiunti peutEntrer e peutEntrerHotel

--- src/implementations/MoteurJeuImpl.java
+++ src/implementations/MoteurJeuImpl.java
@@ -11,35 +11,20 @@ import gui.GuiMainWindow;
 import services.IHotelVille;
 import services.IMine;
 import services.IMoteurJeu;
+import services.IMuraille;
+import services.ITerrain;
 import services.IVillageois;
 import utils.Utils;
 
 public class MoteurJeuImpl implements IMoteurJeu{
-	protected int largeurTerrain;
-    protected int hauteurTerrain;
     protected int maxPasJeu;  
     protected int pasJeuCourant;
     protected boolean estFini;
     protected EResultat resultatFinal;
-    protected ArrayList<IVillageois> villageois;
-    protected ArrayList<IMine> mines;
-    protected GuiMainWindow gui;
-    
-    protected IHotelVille hotelDeVille;
+    protected ITerrain terrain;
     
     public MoteurJeuImpl(){}
     
-
-
-	@Override
-	public int getLargeurTerrain() {
-		return this.largeurTerrain;
-	}
-
-	@Override
-	public int getHauteurTerrain() {
-		return this.hauteurTerrain;
-	}
 
 	@Override
 	public int getMaxPasJeu() {
@@ -61,112 +46,62 @@ public class MoteurJeuImpl implements IMoteurJeu{
 		return this.resultatFinal;
 	}
 
-	@Override
-	public ArrayList<IVillageois> getListVillageois() {
-		return this.villageois;
-	}
-
-	@Override
-	public IVillageois getVillageois(int num) {
-		return this.villageois.get(num);
-	}
-
-	@Override
-	public int getPositionVillageoisX(int num) {
-		return this.getVillageois(num).getX();
-	}
-
-	@Override
-	public int getPositionVillageoisY(int num) {
-		return this.getVillageois(num).getY();
-	}
-
-	@Override
-	public ArrayList<IMine>  getListMines() {
-		return this.mines;
-	}
-
-	@Override
-	public IMine getMine(int num) {
-		return this.mines.get(num);
-		
-	}
-	
-	@Override
-	public int getPositionMineX(int num) {
-		return this.getMine(num).getX();
-	}
-
-	@Override
-	public int getPositionMineY(int num) {
-		return this.getMine(num).getY();
-	}
-
-	@Override
-	public IHotelVille getHotelDeVille() {
-		return this.hotelDeVille;
-	}
-
-	@Override
-	public int getPositionHotelVilleX() {
-		return this.hotelDeVille.getX();
-	}
-
-	@Override
-	public int getPositionHotelVilleY() {
-		return this.hotelDeVille.getY();
-	}
 
 	@Override
 	public boolean peutEntrer(int numVillageois, int numMine) {
-		// TODO implementer
-		return true;
-	}
-
-	@Override
-	public boolean peutEntrerHotelVille(int num) {
-		// TODO implementer
-		return true;
-	}
-
-	@Override
-	public IMoteurJeu init(int largeur, int hauteur, int maxPasJeu) {
-		 this.largeurTerrain = largeur ;                       
-		 this.hauteurTerrain = hauteur;                   
-		 this.maxPasJeu =     maxPasJeu;                            
-		 this.pasJeuCourant= 0;                             
-		 this.estFini = false;                               
+		IVillageois vill = this.getVillageois(numVillageois);
+		IMine mine = this.getMine(numMine);
 		
-		 if (this.hotelDeVille == null)
-				throw new Error("Vous devez binder le Hotel de ville avant de faire appel a init");
-		 if (this.villageois == null)
-				throw new Error("Vous devez binder les villageois avent de faire appel a init");
-		 if (this.mines == null)
-				throw new Error("Vous devez binder les Mines  ville avant de faire appel a init");
-		 
-		 // Initialisation Hotel de ville
-		 int hvx = Utils.randInt(0, 51);
-		 int hvy = Utils.randInt(0, 51);
-		 this.hotelDeVille.init(hvx,hvy, 20, 20, 16);
-		 
-		// Initialisation Villageois
-		int poshx = this.hotelDeVille.getX();
-		int poshy = this.hotelDeVille.getY();
-		for( IVillageois villageois : this.villageois){
-			int vx = poshx + Utils.randInt(0, 51);
-			int vy = poshy + Utils.randInt(0, 51);
-			villageois.init(vx,vy,ERace.HUMAN, 4, 4, 10, 10.0, 100);
+		// Si la mine est laminée retourne false
+		if( mine.estLaminee() ) return false;
+		
+		int mineCenterX = mine.getX() + ( mine.getLargeur() / 2 );
+		int mineCenterY = mine.getY() + ( mine.getHauteur() / 2 );
+		
+		return Utils.distance(	vill.getX(), 
+								vill.getY(),
+								mineCenterX, 
+								mineCenterY) <= 51;
+		
+	}
 
-			
-		}
+	@Override
+	public IMoteurJeu init(ITerrain terrain, int maxPas) {
+		this.terrain = terrain;
+		this.maxPasJeu =     maxPas;                            
+		this.pasJeuCourant= 0;                             
 
-		// Initialisation Mines
-		for( IMine mine: this.mines){
-			mine.init(7,7);
-			mine.setX(Utils.randInt(0, this.getLargeurTerrain()));
-			mine.setY(Utils.randInt(0, this.getLargeurTerrain()));
-
-		}
+		//TODO: à faire dans l'impl de terrain
+////		if (this.hotelDeVille == null)
+////			throw new Error("Vous devez binder le Hotel de ville avant de faire appel a init");
+////		if (this.villageois == null)
+////			throw new Error("Vous devez binder les villageois avent de faire appel a init");
+////		if (this.mines == null)
+////			throw new Error("Vous devez binder les Mines  ville avant de faire appel a init");
+//
+//		// Initialisation Hotel de ville
+//		int hvx = Utils.randInt(0, 51);
+//		int hvy = Utils.randInt(0, 51);
+//		this.hotelDeVille.init(hvx,hvy, 20, 20, 16);
+//
+//		// Initialisation Villageois
+//		int poshx = this.hotelDeVille.getX();
+//		int poshy = this.hotelDeVille.getY();
+//		for( IVillageois villageois : this.villageois){
+//			int vx = poshx + Utils.randInt(0, 51);
+//			int vy = poshy + Utils.randInt(0, 51);
+//			villageois.init(vx,vy,ERace.HUMAN, 4, 4, 10, 10.0, 100);
+//
+//
+//		}
+//
+//		// Initialisation Mines
+//		for( IMine mine: this.mines){
+//			mine.init(7,7);
+//			mine.setX(Utils.randInt(0, this.getLargeurTerrain()));
+//			mine.setY(Utils.randInt(0, this.getLargeurTerrain()));
+//
+//		}
 
 		 
 		 return this;
@@ -185,9 +120,6 @@ public class MoteurJeuImpl implements IMoteurJeu{
 	public void pasJeu(ECommande commande, int numVillageois, int argument) {
 		switch(commande){
 		case DEPLACER:
-			IVillageois v = this.getVillageois(numVillageois);
-			v.setX( (int ) (v.getX() + v.getVitesse()* Math.cos(argument)));
-			v.setY( (int ) (v.getY() + v.getVitesse()* Math.sin(argument)));
 			break;
 		case ENTREHOTELVILLE:
 			break;
@@ -201,31 +133,73 @@ public class MoteurJeuImpl implements IMoteurJeu{
 		
 		this.pasJeuCourant += 1;
 		
-		this.updateGui(this);
-	}
-	
-	private void updateGui(MoteurJeuImpl moteurJeuImpl) {
-		this.gui.updateMoteur(moteurJeuImpl);
-		
 	}
 
-	public void bindWindow(GuiMainWindow gui){
-		this.gui = gui;
+	@Override
+	public ITerrain getTerrain() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public IMuraille getMuraille(int mu) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public IHotelVille getHotelVille(int hdv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public boolean peutEntrerHotelVille(int vilNum, int hdvNum) {
+		IVillageois vill = this.getVillageois(vilNum);
+		IHotelVille hdv = this.getHotelVille(hdvNum);
+		
+		// Si la race n'est pas la bonne retourne false
+		if( hdv.getEtatAppartenance() != vill.getRace() ) return false;
+		
+		int hdvCenterX = hdv.getX() + ( hdv.getLargeur() / 2 );
+		int hdvCenterY = hdv.getY() + ( hdv.getHauteur() / 2 );
+		
+		return Utils.distance(	vill.getX(), 
+								vill.getY(),
+								hdvCenterX, 
+								hdvCenterY) <= 51;
+	}
+
+
+
+	@Override
+	public boolean peutTaperMuraille(int vilNum, int mur) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 
 	@Override
-	public void bind(ArrayList<IVillageois> villageois,
-			ArrayList<IMine> mines) {
-		this.villageois = villageois;
-		this.mines = mines;
+	public IVillageois getVillageois(int vill) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+
 	@Override
-	public void bind(IHotelVille hv) {
-		this.hotelDeVille = hv;
-		
+	public IMine getMine(int mi) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+
+
 
 
 }
