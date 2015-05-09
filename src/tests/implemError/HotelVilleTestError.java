ed440d153d2dc2ae9bddb5c6e2cb40f3b4c64e34
digitex3d <digitex3d@gmail.com>
2015-05-09 10:31:10 +0200

    HV error test

---
+++ src/tests/implemError/HotelVilleTestError.java
@@ -0,0 +1,16 @@
+package tests.implemError;
+
+import implemError.HotelVilleImplError;
+
+import org.junit.Before;
+
+import tests.AbstractHotelVilleTest;
+import contracts.HotelVilleContract;
+
+public class HotelVilleTestError extends AbstractHotelVilleTest{
+	@Override
+	@Before
+	public void before() {
+		hdv = new HotelVilleContract(new HotelVilleImplError());
+	}
+}
